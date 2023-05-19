6e9457ef0f9b6e48ad7eafcb26fadab0443d708d
praveennick <praveennick1@gmail.com>
2023-05-20 01:21:28 +0530

    added some more changes

---
+++ index.js
@@ -0,0 +1,28 @@
+//slider
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    spaceBetween: 30,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    effect: 'coverflow',
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+  });
+
+// overlay button
+
+const overlayButtons = document.querySelectorAll('.overlay-button');
+overlayButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const url = button.getAttribute('data-url');
+    window.open(url, '_blank');
+  });
+});
