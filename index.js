c6a9c38322839e54d3ab9cb1db035ef2cbda2d72
praveennick <praveennick1@gmail.com>
2023-06-13 15:47:45 +0530

    added new changes

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 //slider
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 'auto',
-    centeredSlides: true,
-    spaceBetween: 30,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    effect: 'coverflow',
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-  });
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+});
 
 // overlay button
 
-const overlayButtons = document.querySelectorAll('.overlay-button');
-overlayButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const url = button.getAttribute('data-url');
-    window.open(url, '_blank');
+const overlayButtons = document.querySelectorAll(".overlay-button");
+overlayButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const url = button.getAttribute("data-url");
+    window.open(url, "_blank");
   });
 });
+
+var width = screen.width;
+var height = screen.height;
+
+console.log("hello", width + "*" + height);
